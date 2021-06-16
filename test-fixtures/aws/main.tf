67e71aa82700aea76f6a5cfb642416b220457e81
Mahmoud Ismail <mahmoud@logicalclocks.com>
2021-06-16 16:36:40 +0200

    aws_s3_bucket_public_access_block should depend on aws_s3_bucket

--- test-fixtures/aws/main.tf
+++ test-fixtures/aws/main.tf
@@ -55,7 +55,7 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_public_access_block" "block_bucket" {
   count                   = var.num_buckets
-  bucket                  = "${var.bucket_name_prefix}-${count.index}"
+  bucket                  = aws_s3_bucket.bucket[count.index].id
   block_public_acls       = true
   block_public_policy     = true
   restrict_public_buckets = true
