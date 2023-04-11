359d06c1d55224634d19558b10c45604ce8ca1e1
Mahmoud Ismail <mahmoud@hopsworks.ai>
2023-04-11 16:31:38 +0200

    [CLOUD-538] Update helpers module version

--- examples/provider/provider_aws.tf
+++ examples/provider/provider_aws.tf
@@ -28,7 +28,7 @@ provider "hopsworksai" {
 module "aws" {
   source  = "logicalclocks/helpers/hopsworksai//modules/aws"
   region  = var.region
-  version = "2.1.0"
+  version = "2.3.0"
 }
 
 # Step 2: create a cluster with 1 worker
